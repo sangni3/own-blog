5cfa0ba943f1eb0c3a4932873319147fcb102b42
sangni3 <63947751+sangni3@users.noreply.github.com>
2022-01-17 21:26:02 +0800

    Delete until1 directory

--- until1/Blog/web/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from .models import ArticleComment
-
-
-class CommentForm(forms.ModelForm):
-    class Meta:
-        model = ArticleComment
-        fields = ['user', 'content', 'blog']
